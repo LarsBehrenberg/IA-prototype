e65ac32823a950110542268c1201c9f0298e9ac4
Lars Behrenberg <l.behrenberg@gmail.com>
2020-04-16 19:22:48 +0900

    fix links to IA

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => (
     <footer className="site-footer">
         <div className="container">
-        <div className="col-md-4"><a className="btn btn-white-outline" href="https://impressionistarts.com/most-expensive-impressionist-paintings.html">Dearest impressionist paintings</a></div>
-        <div className="col-md-4"><a className="btn btn-white-outline" href="https://impressionistarts.com/impressionism-timeline.html">Impressionist
+        <div className="col-md-4"><a className="btn btn-white-outline" href="#top">Dearest impressionist paintings</a></div>
+        <div className="col-md-4"><a className="btn btn-white-outline" href="#top">Impressionist
             Timeline</a></div>
-        <div className="col-md-4"><a className="btn btn-white-outline" href="https://impressionistarts.com/impressionism-quiz.html">
+        <div className="col-md-4"><a className="btn btn-white-outline" href="#top">
             Impressionist Quiz</a></div>
         </div>
     </footer>
