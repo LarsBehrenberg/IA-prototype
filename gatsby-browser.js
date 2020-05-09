69ae5ca92728ae803ab3cb0010a6f4455de0e257
Lars Behrenberg <l.behrenberg@gmail.com>
2020-05-09 13:36:16 +0900

    simple image carousel

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,48 @@
 /* eslint-disable */
 
+let myTimer
+
+export const onPreRouteUpdate = ({ location, prevLocation }) => {
+  clearInterval(myTimer)
+}
+
 export const onRouteUpdate = ({ location, prevLocation }) => {
   document.getElementsByTagName('body')[0].style.overflow = 'auto'
+
+  let slideIndex = 1
+
+  clearInterval(myTimer)
+  function showSlides(n) {
+    var i
+    var carouselSlides = document.getElementsByClassName('carousel-slides')
+    if (n > carouselSlides.length) {
+      slideIndex = 1
+    }
+    if (n < 1) {
+      slideIndex = carouselSlides.length
+    }
+    for (i = 0; i < carouselSlides.length; i++) {
+      carouselSlides.item(i).style.display = 'none'
+    }
+    if (carouselSlides.length !== 0) {
+      carouselSlides.item(slideIndex - 1).style.display = 'block'
+      carouselSlides.item(slideIndex - 1).style.animation = 'fadeInHero 7s'
+    }
+  }
+
+  const plusSlides = n => {
+    if (n < 0) {
+      showSlides((slideIndex -= 1))
+    } else {
+      showSlides((slideIndex += 1))
+    }
+  }
+  if (location.pathname === '/') {
+    showSlides(slideIndex)
+    myTimer = setInterval(function () {
+      plusSlides(1)
+    }, 7000)
+  }
 }
 
 export const onClientEntry = () => {
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import styled from '@emotion/styled'
 import Img from 'gatsby-image'
 import { graphql, useStaticQuery, Link } from 'gatsby'
-const Wrapper = styled.article`
+
+const Carousel = styled.div`
+  width: 100%;
+  height: 450px;
+`
+
+const Wrapper = styled.div`
   margin-top: 20px;
   margin-bottom: 2rem;
   position: relative;
   z-index: 10;
   box-shadow: 0 15px 25px rgba(0, 0, 0, 0.2);
-  height: 450px;
+  height: 100%;
   width: 100%;
+  display: none;
 `
 
 const StyledLink = styled(Link)`
@@ -52,7 +59,10 @@ const Image = styled.div`
   left: 0;
   bottom: 0;
   z-index: 1;
+  visibility: visible;
   object-fit: cover;
+  animation: fadeInHero 7s ease-in-out;
+  transition: opacity 1s linear;
   > div {
     position: static !important;
   }
@@ -63,6 +73,8 @@ const Image = styled.div`
 
 const Info = styled.div`
   color: #f0f0f0;
+
+  animation: fadeInHero 7s ease-in-out;
   span {
     font-size: 2.6rem;
   }
@@ -91,6 +103,11 @@ const Title = styled.h2`
   font-weight: 400;
 `
 
+const StopButton = styled.button`
+  margin: 0 0 0 auto;
+  z-index: 100;
+`
+
 const Hero = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -101,92 +118,67 @@ const Hero = () => {
           }
         }
       }
+      allFile(
+        filter: { extension: { eq: "jpg" }, name: { regex: "/night/g" } }
+      ) {
+        nodes {
+          childImageSharp {
+            fluid(maxWidth: 1200, quality: 90, traceSVG: { color: "#2B2B2F" }) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
     }
   `)
+
+  const { nodes } = data.allFile
+
+  // const stopAnimation = () => {
+  //   document.getElementsByClassName(
+  //     'carousel-slides'
+  //   ).style.animationPlayState = 'paused'
+  // }
+
+  console.log(nodes)
   return (
-    // <Wrapper>
-    //   <ImageWrapper to="gallery">
-    //     <Img
-    //       fluid={data.file.childImageSharp.fluid}
-    //     />
-    //   </ImageWrapper>
-    //   <TextContainer>
-    //     <h1>
-    //       I don't like art. Not most of it. <br />
-    //       But I have always loved{' '}
-    //       <span style={{ color: '#e53132' }}>impressionism</span>. Always.
-    //     </h1>
-    //   </TextContainer>
-    // </Wrapper>
-    <Wrapper>
-      <Image>
-        <Img fluid={data.file.childImageSharp.fluid} />
-      </Image>
-      <StyledLink to={'path'}>
-        <Info>
-          {/* <span>{date}</span> */}
-          <Title>About us</Title>
-          <span>
-            I don't like art. Not most of it. <br />
-            But I have always loved
-            <br />
-            <span style={{ color: '#e53132' }}>impressionism</span>. Always.
-          </span>
-        </Info>
-      </StyledLink>
-    </Wrapper>
+    <Carousel className="carousel">
+      <Wrapper className="carousel-slides">
+        <Image>
+          <Img fluid={data.file.childImageSharp.fluid} />
+        </Image>
+        <StyledLink to="/about-us">
+          <Info>
+            {/* <span>{date}</span> */}
+            <Title>About us</Title>
+            <span>
+              I don't like art. Not most of it. <br />
+              But I have always loved
+              <br />
+              <span style={{ color: '#e53132' }}>impressionism</span>. Always.
+            </span>
+          </Info>
+        </StyledLink>
+      </Wrapper>
+      {nodes.map((node, index) => {
+        return (
+          <Wrapper className="carousel-slides">
+            <Image>
+              <Img fluid={node.childImageSharp.fluid} />
+            </Image>
+            <StyledLink to={index === 0 ? '/impressionism-quiz' : '/gallery'}>
+              <Info>
+                {/* <span>{date}</span> */}
+                <Title>{index === 0 ? 'Our Quiz' : ' The Gallery'}</Title>
+                <span>Text Here</span>
+              </Info>
+            </StyledLink>
+          </Wrapper>
+        )
+      })}
+      {/* <StopButton onClick={stopAnimation}>Stop animation</StopButton> */}
+    </Carousel>
   )
 }
 
 export default Hero
-
-// const Wrapper = styled.div`
-//   margin: 0;
-//   height: 300px;
-// `
-// const TextContainer = styled.div`
-//   & h1 {
-//     font-size: 3rem;
-//   }
-
-//   position: absolute;
-//   top: 0;
-//   left: 0;
-//   right: 0;
-//   bottom: 0;
-//   display: flex;
-//   flex-direction: row;
-//   justify-content: space-between;
-//   padding: 1rem;
-//   z-index: 3;
-//   border-radius: 0.4rem;
-//   &:after {
-//     content: '';
-//     position: absolute;
-//     display: block;
-//     width: 100%;
-//     height: 100%;
-//     top: 0;
-//     left: 0;
-//     right: 0;
-//     bottom: 0;
-//     background: linear-gradient(
-//       to right,
-//       rgba(0, 0, 0, 0) 0%,
-//       rgba(0, 0, 0, 0.3) 50%,
-//       rgba(0, 0, 0, 0.7) 80%,
-//       rgba(0, 0, 0, 1) 100%
-//     );
-//   }
-// `
-
-// const ImageWrapper = styled(Link)`
-//   margin: 0 auto;
-//   position: absolute;
-//   top: 60;
-//   z-index: -1;
-//   left: 0;
-//   width: 100%;
-//     z-index: -1;
-//   }
-// `
