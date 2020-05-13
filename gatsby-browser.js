4cbbc8dfc75e72ea9a81742bfd11a05f9584ddfd
Lars Behrenberg <l.behrenberg@gmail.com>
2020-05-14 08:14:49 +0900

    remove commented code

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,57 +1,17 @@
 /* eslint-disable */
 
-let myTimer
-
-// export const onPreRouteUpdate = ({ location, prevLocation }) => {
-//   clearInterval(myTimer)
-// }
-
+// Prevent/remove any style changes from Lightbox onRouteUpdate
 export const onRouteUpdate = ({ location, prevLocation }) => {
   document.getElementsByTagName('body')[0].style.overflow = 'auto'
-
-  // let slideIndex = 1
-
-  // clearInterval(myTimer)
-  // function showSlides(n) {
-  //   var i
-  //   var carouselSlides = document.getElementsByClassName('carousel-slides')
-  //   if (n > carouselSlides.length) {
-  //     slideIndex = 1
-  //   }
-  //   if (n < 1) {
-  //     slideIndex = carouselSlides.length
-  //   }
-  //   for (i = 0; i < carouselSlides.length; i++) {
-  //     carouselSlides.item(i).style.display = 'none'
-  //   }
-  //   if (carouselSlides.length !== 0) {
-  //     carouselSlides.item(slideIndex - 1).style.display = 'block'
-  //     carouselSlides.item(slideIndex - 1).style.animation = 'fadeInHero 7s'
-  //   }
-  // }
-
-  // const plusSlides = n => {
-  //   if (n < 0) {
-  //     showSlides((slideIndex -= 1))
-  //   } else {
-  //     showSlides((slideIndex += 1))
-  //   }
-  // }
-  // if (location.pathname === '/') {
-  //   showSlides(slideIndex)
-  //   myTimer = setInterval(function () {
-  //     plusSlides(1)
-  //   }, 7000)
-  // }
 }
 
+// Disable Sidebar on post pages when scrolling down to the newsletter
 export const onClientEntry = () => {
   window.onload = () => {
     window.addEventListener('scroll', function (event) {
       if (document.getElementById('sidebar')) {
-        const sideBar = document.getElementById('sidebar') // your element
+        const sideBar = document.getElementById('sidebar')
         const sideBottom = sideBar.getBoundingClientRect().bottom + 100
-        // current scroll position relative to the body
         const footerDiv = document.getElementById('newsletter')
         const footerTop = footerDiv.getBoundingClientRect().top - 50
         if (sideBottom > footerTop) {
