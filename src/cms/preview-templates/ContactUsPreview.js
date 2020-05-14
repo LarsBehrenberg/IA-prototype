0a48b5aa0ddc14a0141d17e6d5f3b0b2cfa991fd
Lars Behrenberg <l.behrenberg@gmail.com>
2020-05-14 15:56:26 +0900

    fix preview styling

--- src/cms/preview-templates/ContactUsPreview.js
+++ src/cms/preview-templates/ContactUsPreview.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import '../../styles/all.css'
+import '../preview-styles/contact-preview.css'
 
 const ContactUsPreview = ({ entry, widgetFor, getAsset }) => {
   let image = entry.getIn(['data', 'backgroundImage'])
