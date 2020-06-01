253a3e858ed27ae8d87ff4efff8f3d688324988a
Lars Behrenberg <l.behrenberg@gmail.com>
2020-06-01 09:45:59 +0900

    Remove User Description

--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -11,20 +11,19 @@ import galleryStyles from '../styles/galleryStyles'
 const Gallery = ({ data }) => {
   const { edges } = data.allInstaNode
   const nodeURL = 'https://www.instagram.com/p'
-  const {
-    full_name,
-    username,
-    biography,
-    profile_pic_url_hd,
-    edge_follow,
-    edge_followed_by,
-  } = data.instaUserNode
+  // const {
+  //   full_name,
+  //   username,
+  //   biography,
+  //   profile_pic_url_hd,
+  //   edge_follow,
+  //   edge_followed_by,
+  // } = data.instaUserNode
 
   return (
     <Layout>
       <SEO
         title="Gallery | ImpressionistArts.com"
-        description={`Welcome to the ImpressionistArts Gallery || ${biography} ...`}
         banner={edges[0].node.localFile.childImageSharp.fixed.src}
       />
       <div
@@ -33,7 +32,7 @@ const Gallery = ({ data }) => {
           ${galleryStyles}
         `}
       >
-        <div className="gallery-container">
+        {/* <div className="gallery-container">
           <div className="profile">
             <div className="profile-image">
               <img src={profile_pic_url_hd} alt="Instagram profile" />
@@ -75,8 +74,8 @@ const Gallery = ({ data }) => {
               </p>
             </div>
           </div>
-        </div>
-        <div className="gallery-container">
+        </div> */}
+        <div className="gallery-container" style={{ paddingTop: '80px' }}>
           <div className="gallery">
             {edges.map(({ node }) => {
               return (
@@ -98,13 +97,35 @@ const Gallery = ({ data }) => {
                     <ul>
                       <li className="gallery-item-likes">
                         <span role="img" aria-label="heart">
-                          &#128150;
+                          <svg
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 24 24"
+                            fill="white"
+                            style={{
+                              fontSize: '14px',
+                              lineHeight: '1.45',
+                            }}
+                          >
+                            <path d="M12 4.435C10.011-.964 0-.162 0 8.003 0 12.071 3.06 17.484 12 23c8.94-5.516 12-10.929 12-14.997C24-.115 14-.996 12 4.435z"></path>
+                          </svg>
                         </span>{' '}
                         {node.likes !== null ? node.likes.toLocaleString() : 0}
                       </li>
                       <li className="gallery-item-comments">
                         <span role="img" aria-label="speech-balloon">
-                          &#128172;
+                          <svg
+                            width="1em"
+                            height="1em"
+                            viewBox="0 0 24 24"
+                            fill="white"
+                            style={{
+                              fontSize: '14px',
+                              lineHeight: '1.45',
+                            }}
+                          >
+                            <path d="M24 9.874C24 4.42 18.627 0 12 0S0 4.42 0 9.874c0 4.512 3.678 8.317 8.701 9.496L12 24l3.299-4.63C20.322 18.19 24 14.385 24 9.874z"></path>
+                          </svg>
                         </span>{' '}
                         {node.comments !== null
                           ? node.likes.toLocaleString()
@@ -127,18 +148,6 @@ export default Gallery
 
 export const query = graphql`
   query {
-    instaUserNode {
-      full_name
-      username
-      biography
-      profile_pic_url_hd
-      edge_follow {
-        count
-      }
-      edge_followed_by {
-        count
-      }
-    }
     allInstaNode(sort: { fields: timestamp, order: DESC }, limit: 12) {
       edges {
         node {
